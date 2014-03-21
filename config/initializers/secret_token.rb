cf5feef68971d5c4246989e79969365ebdce3e95
Mitch24 <paulanthony1920@gmail.com>
2014-03-21 10:44:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grocery::Application.config.secret_key_base = '3194dcccc91e6b7245ec63bae964afdf3774ca38fc844d0f0001f50f51e97094a59fe7da6ccb3526f56297be38047feead5386f5749adf9e1179f44db0eb9a5f'
