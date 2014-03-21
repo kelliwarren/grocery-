e18f8e3a9aee9b31d9673fbc29c484135e375762
Mithc24 <paulanthony1920@gmail.com>
2014-03-21 16:14:43 -0700

    working on passing test

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,4 @@
 class Store < ActiveRecord::Base
+  validates_presence_of :street_address, :city, :name, :zip, :user_id
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
- 
+  validates_presence_of :name, :phone
   has_many :stores
 end
