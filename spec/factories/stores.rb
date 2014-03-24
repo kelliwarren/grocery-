34ae5d843cc2f5a1b2acdc35728c1d7c1788fbfc
Mitch24 <paulanthony1920@gmail.com>
2014-03-24 09:19:15 -0700

    fixes typo in factory/store

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :store do
     name { Faker::Name.name }
-    # user_id rand(1..150)a store belongs to a user, so we need to actually create one in this factory.  user {create(:user)}
     street_address {Faker::AddressUS.street_address}
     city { Faker::AddressUS.city}
     state { Faker::AddressUS.state}
