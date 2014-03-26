7e65ad42f35a90a0392411b7fae4c0be653c518a
kelliwarren <kmdesigngroup@gmail.com>
2014-03-26 12:19:27 -0700

    product controller broken

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -12,43 +12,63 @@ describe ProductsController do
     end
   end
   describe '#show' do 
-    it 'should allow all users regardless of being signed in or store owner to access the index action' do 
+    it "should be available to show view" do
       user = create(:user, store_owner: true)
-      product = create(:product)
       store = create(:store, user_id: user.id)
-      store.products << product
-      get :index, {user_id: user, store_id: store}
-      expect(assigns(@products)).to_not be_empty
+      get :show, user_id: user.id, id: store.id
+      response.should be_success
     end
   end
-  describe '#index' do 
-    it 'should allow all users regardless of being signed in or store owner to access the index action' do 
+
+  describe '#create'do
+    it 'should not allow a user who is not a store owner to create a store' do
+      user = create(:user)
+      store = build(:store)
+      post :create, {user_id: user.id, :store => store.attributes }
+      expect(user.stores.count).to eq(0)
+    end
+
+    it 'should allow a user who is a store_owner to create a store' do
       user = create(:user, store_owner: true)
-      product = create(:product)
-      store = create(:store, user_id: user.id)
-      store.products << product
-      get :index, {user_id: user, store_id: store}
-      expect(assigns(@products)).to_not be_empty
+      store = build(:store)
+      post :create, {user_id: user.id, :store => store.attributes }
+      expect(user.stores.count).to eq(1)
     end
   end
-  describe '#index' do 
-    it 'should allow all users regardless of being signed in or store owner to access the index action' do 
+
+  describe '#update' do
+    it 'should not all a user who is not a store owner to update a store' do
+      user = create(:user)
+      store = create(:store)
+      user.stores << store
+      put :update, { user_id: store.user.id, id: store.id, :store =>{name: 'new name'} }
+      expect(user.stores.last.name).to_not eq('new name')
+    end
+    it 'should allow a user.store_owner to update a record' do
       user = create(:user, store_owner: true)
-      product = create(:product)
-      store = create(:store, user_id: user.id)
-      store.products << product
-      get :index, {user_id: user, store_id: store}
-      expect(assigns(@products)).to_not be_empty
+      store = create(:store)
+      user.stores << store
+      put :update, { user_id: store.user.id, id: store.id, :store => {name: 'new name'} }
+      expect(user.stores.last.name).to eq('new name')
     end
   end
-  describe '#index' do 
-    it 'should allow all users regardless of being signed in or store owner to access the index action' do 
+
+  describe '#destroy' do
+    it 'should not allow a user who is not a store owner to delete a store' do
+      user = create(:user)
+      store = create(:store)
+      user.stores << store
+      delete :destroy,{ user_id: store.user.id, id: store.id }
+      expect(user.stores.count).to eq(1)
+    end
+    
+    it 'should allow a user.store_owner to delete a store' do
       user = create(:user, store_owner: true)
-      product = create(:product)
-      store = create(:store, user_id: user.id)
-      store.products << product
-      get :index, {user_id: user, store_id: store}
-      expect(assigns(@products)).to_not be_empty
+      store = create(:store)
+      user.stores << store
+      delete :destroy,{ user_id: store.user.id, id: store.id }
+      expect(user.stores.count).to eq(0)
     end
   end
+
 end
