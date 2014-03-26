7ae257049ce02bb902ce30607d938112bf97d54f
ageobab <agbabiki@gmail.com>
2014-03-26 12:28:49 -0400

    product views with correct routes, factories for product temp canceled out

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,11 +1,23 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+#FactoryGirl.define do
+  #factory :product do
+    #name { Faker::Product.product_name }
+   # description:{ Faker::
+   # price: rand(5..278),
+   # quantity: rand(0..100)
+    #image "MyString"
+   # description "MyText"
+ # end
+#end
+
 FactoryGirl.define do
-  factory :product do
-    name "MyString"
-    price 9.99
-    quantity 1
-    image "MyString"
-    description "MyText"
+  factory :store do
+    name { Faker::Name.name }
+    street_address {Faker::AddressUS.street_address}
+    city { Faker::AddressUS.city}
+    state { Faker::AddressUS.state}
+    zip { Faker::AddressUS.zip_code}
+    user { create(:user) }
   end
 end
