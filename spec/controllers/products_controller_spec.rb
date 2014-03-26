5ca96646730669abb50da630b9480665a1e097a4
kelliwarren <kmdesigngroup@gmail.com>
2014-03-26 12:51:26 -0700

    product controller tests

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ProductsController do
-  describe '#index', :focus do 
+  describe '#index' do 
     it 'should allow all users regardless of being signed in or store owner to access the index action' do 
       user = create(:user, store_owner: true)
       product = create(:product)
@@ -11,11 +11,12 @@ describe ProductsController do
       expect(assigns(@products)).to_not be_empty
     end
   end
-  describe '#show' do 
+  describe '#show', :focus do 
     it "should be available to show view" do
       user = create(:user, store_owner: true)
-      product = create(:product, user_id: user.id)
-      get :show, user_id: user.id, id: product.id
+      store = create(:store, user_id: user.id)
+      product = create(:product, store_id: store.id)
+      get :show, user_id: user.id, store_id: store.id, id: product.id
       response.should be_success
     end
   end
@@ -23,42 +24,47 @@ describe ProductsController do
   describe '#create'do
     it 'should not allow a user who is not a store owner to create a product' do
       user = create(:user)
+      store = create(:store, user_id: user.id)
       product = build(:product)
-      post :create, {user_id: user.id, :product => product.attributes }
+      post :create, {user_id: user.id, store_id: store.id, :product => product.attributes }
       expect(user.products.count).to eq(0)
     end
 
     it 'should allow a user who is a store_owner to create a product' do
       user = create(:user, store_owner: true)
-      store = build(:product)
-      post :create, {user_id: user.id, :product => product.attributes }
+      store = create(:store, user_id: user.id)
+      product = build(:product)
+      post :create, {user_id: user.id, store_id: store.id, :product => product.attributes }
       expect(user.products.count).to eq(1)
     end
   end
 
   describe '#update' do
-    it 'should not all a user who is not a store owner to update a store' do
+    it 'should not allow a user who is not a store owner to update a product' do
       user = create(:user)
       store = create(:store)
-      user.stores << store
-      put :update, { user_id: store.user.id, id: store.id, :store =>{name: 'new name'} }
-      expect(user.stores.last.name).to_not eq('new name')
+      product = create(:product)
+      store.products << product
+      put :update, { user_id: store.user.id, store_id: store.id, id: product.id, :product =>{name: 'new name'} }
+      expect(user.store.product.last.name).to_not eq('new name')
     end
     it 'should allow a user.store_owner to update a record' do
-      user = create(:user, store_owner: true)
+      user = create(:user)
       store = create(:store)
-      user.stores << store
-      put :update, { user_id: store.user.id, id: store.id, :store => {name: 'new name'} }
+      product = create(:product)
+      store.products << product
+      put :update, { user_id: store.user.id, store_id: store.id, id: product.id, :product =>{name: 'new name'} }
       expect(user.stores.last.name).to eq('new name')
     end
   end
 
   describe '#destroy' do
-    it 'should not allow a user who is not a store owner to delete a store' do
+    it 'should not allow a user who is not a store owner to delete a product' do
       user = create(:user)
       store = create(:store)
-      user.stores << store
-      delete :destroy,{ user_id: store.user.id, id: store.id }
+      product = create(:product)
+      store.products << product
+      delete :destroy,{ user_id: store.user.id, store_id: store.id, id: product.id }
       expect(user.stores.count).to eq(1)
     end
     
@@ -66,7 +72,7 @@ describe ProductsController do
       user = create(:user, store_owner: true)
       store = create(:store)
       user.stores << store
-      delete :destroy,{ user_id: store.user.id, id: store.id }
+      delete :destroy,{ user_id: store.user.id, store_id: store.id, id: product.id }
       expect(user.stores.count).to eq(0)
     end
   end
