abf51d14c28417163bfef8251f22eb8abea7db9d
kelliwarren <kmdesigngroup@gmail.com>
2014-03-27 12:44:57 -0700

    oder model and association built

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,16 +2,10 @@ class Product < ActiveRecord::Base
 
   validates :name, :description, :quantity, :store_id, :image, :price, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
- # validates :name, :uniqueness => true
+  # validates :name, :uniqueness => true
   validates :image, :format => {
     :with    => %r{\.(gif|jpg|png)\Z}i,
   :message => 'must be a URL for GIF, JPG or PNG image.' }
-
   belongs_to :store
-<<<<<<< HEAD
   has_and_belongs_to_many :orders
- 
-=======
- has_and_belongs_to_many :orders
->>>>>>> abf23a8a8ec88d5b85ba0b3827453b0a087b114f
 end
