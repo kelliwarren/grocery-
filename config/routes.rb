80e4c00025c8e5c041db9ab5e642aea808d6a7c4
Mitch24 <paulanthony1920@gmail.com>
2014-03-28 10:49:55 -0700

    order controller routes for user and store

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Grocery::Application.routes.draw do
   root to: "users#index"
+  resources :users do
+    resources :order
+  end
+  resources :stores do
+    resources :order
+  end
   devise_for :users
   resources :stores, only: [:show]
   resources :users do
