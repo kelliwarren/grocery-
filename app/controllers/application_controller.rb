eaedaba658572d441911856cfef4c9bf72e8460d
Mitch24 <paulanthony1920@gmail.com>
2014-03-31 11:27:06 -0700

    add product to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    session[:product] = @product.id
+    redirect_to root_path
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name 
+    devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:sign_up) << :phone
   end
 end
