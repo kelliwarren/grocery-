f7b469327efb142b183c53678810c06d190572c1
kelliwarren <kmdesigngroup@gmail.com>
2014-03-31 12:26:30 -0700

    checkout controller application controller application views

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,8 @@
+class CheckoutController < ApplicationController
+
+  def index
+    @products = Product.where(:id => session[:product])
+  end
+
+#create action we want to a before create authentiate user so they log in before checkout process
+end
