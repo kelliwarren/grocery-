a5a1a86d07e2cc2675b1658fafe50dd8360c17b3
Mitch24 <paulanthony1920@gmail.com>
2014-04-01 07:57:32 -0700

    creates sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140327193435) do
+ActiveRecord::Schema.define(version: 20140401144925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20140327193435) do
     t.integer  "store_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "sessions", force: true do |t|
   end
 
   create_table "stores", force: true do |t|
