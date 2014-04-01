5669c43640019f1eda8e14b8ffcb87cd676c83d1
Mitch24 <paulanthony1920@gmail.com>
2014-04-01 09:36:44 -0700

    creates line_item controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,5 +7,5 @@ class Product < ActiveRecord::Base
     :with    => %r{\.(gif|jpg|png)\Z}i,
   :message => 'must be a URL for GIF, JPG or PNG image.' }
   belongs_to :store
-  has_and_belongs_to_many :orders
+  has_many :line_items
 end
