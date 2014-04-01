f88aebeda8bf56bdd1737f34d1d45617c7e37d4d
Mitch24 <paulanthony1920@gmail.com>
2014-04-01 09:30:05 -0700

    creates associations and migrates line_item model

--- db/migrate/20140401162700_create_line_items.rb
+++ db/migrate/20140401162700_create_line_items.rb
@@ -1,6 +1,8 @@
 class CreateLineItems < ActiveRecord::Migration
   def change
     create_table :line_items do |t|
+      t.belongs_to :product
+      t.belongs_to :order
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401144925) do
+ActiveRecord::Schema.define(version: 20140401162700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "orders", force: true do |t|
     t.integer  "store_id"
